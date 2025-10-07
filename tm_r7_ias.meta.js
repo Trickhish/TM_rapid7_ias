1326a802b341c6adfe40e09ada491d8d3c0eb8fe
Charles-Ivan Dury <35201179+Trickhish@users.noreply.github.com>
2025-10-07 15:34:12 +0200

    Create tm_r7_ias.meta.js

---
+++ tm_r7_ias.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Mon Extension Personnalisée
+// @namespace    http://votredomaine.com/
+// @version      1.0.0
+// @description  Description de votre extension
+// @author       Votre nom
+// @match        https://site-a-modifier.com/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/votre-username/votre-repo/main/mon-script.meta.js
+// @downloadURL  https://raw.githubusercontent.com/votre-username/votre-repo/main/mon-script.user.js
+// @run-at       document-end
+// ==/UserScript==
